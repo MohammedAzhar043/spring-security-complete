a29d87f725a18f7fda7c96df4507046913d8e0d1
MohammedAzhar043 <146701964+MohammedAzhar043@users.noreply.github.com>
2025-02-14 21:16:57 -0800

    adding our own security configuration

--- src/main/java/com/example/securitydemo/SecuritydemoApplication.java
+++ src/main/java/com/example/securitydemo/SecuritydemoApplication.java
@@ -3,6 +3,8 @@ package com.example.securitydemo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
+
 @SpringBootApplication
 public class SecuritydemoApplication {
 
